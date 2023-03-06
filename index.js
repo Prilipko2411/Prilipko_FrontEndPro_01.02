6b88ccdf55631adf5262423ad21bf5da4631f192
Prilipko2411 <prilipko2411@gmail.com>
2023-03-06 21:43:02 +0200

    first

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let name = prompt ('What is your name?')
+let surname = prompt ('What is your surname?')
+let age = prompt ('How old are you?')
+let num = Number(age)
+let city = prompt ('What city are you from?')
+
+result = confirm ('Do you give permission to use your data?')
+
+result ? alert (`Name: ${name}; Surname: ${surname}; Age: ${age}; City: ${city}.`) : 
+alert ('A user has forbidden to use his data.')
