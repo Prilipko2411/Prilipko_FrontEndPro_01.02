fd9132a074c12d0a35109cd391e8082b7802b8bd
Prilipko2411 <prilipko2411@gmail.com>
2023-03-10 22:43:15 +0200

    second

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ result = confirm ('Do you give permission to use your data?')
 
 result ? alert (`Name: ${name}; Surname: ${surname}; Age: ${age}; City: ${city}.`) : 
 alert ('A user has forbidden to use his data.')
+
